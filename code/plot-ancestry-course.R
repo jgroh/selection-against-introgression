8762086bcad3b70f9cc17b31191d0d610e645fc4
Jeff Groh <brogroh@gmail.com>
2020-08-11 11:46:17 -0700

    call plot script in job script

--- code/plot-ancestry-course.R
+++ code/plot-ancestry-course.R
@@ -1,14 +1,42 @@
+#!/usr/bin/Rscript --vanilla
+
 library(tidyverse)
 library(tidyselect)
 
-a <- read.table("results/single-chrom/scheme01/ancestry-results.txt", row.names = 1)
+args <- commandArgs(trailingOnly = TRUE)
+scheme <- args[1]
+filename <- paste0("results/single-chrom/", scheme, "/ancestry-results.txt")
+
+a <- read.table(filename, row.names = 1)
+#a <- read.table("results/single-chrom/scheme01/ancestry-results.txt", row.names = 1)
 a <- as.data.frame(t(a))
 a$pos <- seq(1,1000)
 
+
+# plot frequency trajectory directly
+
+## reorganize data
+b <- a %>%
+  gather(key = rep_gen,
+         value = freq, -pos) %>%
+  separate(rep_gen, c("rep.id", "gen"), sep = "-gen")
+
+## make plot 
+b %>%
+  ggplot(aes(pos, freq, colour=rep.id)) +
+  #geom_point() +
+  geom_smooth(method = "loess", span = .1, se = FALSE) +
+  facet_grid(gen~.)
+
+ggsave(paste0("results/plots/", scheme, "-freq-trajectory.pdf"))
+
+
+
+# plot frequency differentials between time steps
+
+## calculate differentials 
+
 gens <- c("gen005","gen010","gen050","gen100","gen500")
-
-
-  
 for(i in 1:10) {
   # loop over replicates
   
@@ -18,13 +46,13 @@ for(i in 1:10) {
     # select appropriate columns to take difference
     freq2 <- a[paste0("replicate-", i, "-", gens[j+1])]
     
-    if (j != 0) {
-      freq1 <- a[paste0("replicate-", i, "-", gens[j])]
-      n <- paste0("r", i, "_", gens[j+1], "-", gens[j])
-    
-    } else {
+    if (j == 0) {
       freq1 <- rep(0.2, 1000)
       n <- paste0("r", i, "_", gens[j+1], "-", "gen000")
+    
+    } else {
+      freq1 <- a[paste0("replicate-", i, "-", gens[j])]
+      n <- paste0("r", i, "_", gens[j+1], "-", gens[j])
     }
     
     # calculate frequency difference for time step and add new column
@@ -33,17 +61,21 @@ for(i in 1:10) {
   }
 }
 
-# reorganize data
-b <- a %>%
+
+## reorganize data
+d <- a %>%
   select(-contains("replicate")) %>% # removes cols with raw freq values
   gather(key = time.step.id,
          value = delta.freq, -pos) %>%
   separate(time.step.id, c("rep.id", "step.num"), sep = "_")
 
-# make plot 
-b %>%
+## make plot 
+d %>%
   ggplot(aes(pos, delta.freq, colour=rep.id)) +
   #geom_point() +
   geom_smooth(method = "loess", span = .1, se = FALSE) +
   facet_grid(step.num~.)
+
+ggsave(paste0("results/plots/", scheme, "delta-freq-trajectory.pdf"))
+
   
--- code/single-chrom-replicates.sh
+++ code/single-chrom-replicates.sh
@@ -2,7 +2,7 @@
 # run script from top-level project directory
 
 # modify number in tail command to run different schemes
-read scheme rates ends <<< $(cat code/recomb-schemes.txt | tail -n +1 | head -n1) 
+read scheme rates ends <<< $(cat code/recomb-schemes.txt | tail -n +3 | head -n1) 
 export scheme rates ends
 
 # where to write output
@@ -11,7 +11,7 @@ mkdir -p ${outPath}
 
 # modify number after seq to run different number of replicates
 # modify number after parallel to allow for x number of jobs to run at once if multiple corers are available
-seq 2 10 | parallel -q -j 1 \
+seq 1 10 | parallel -q -j 1 \
         slim \
         -l \
             -d seed={} \
@@ -21,6 +21,7 @@ seq 2 10 | parallel -q -j 1 \
             -d ends=${ends} \
             -d "outPath='${outPath}'" \
             code/single-chrom.slim &&
-            find ${outPath} -name "*.trees" | xargs -I{} python code/ancestry.py {} >> ${outPath}/ancestry-results.txt
+            find ${outPath} -name "*.trees" | xargs -I{} python code/ancestry.py {} >> ${outPath}/ancestry-results.txt &&
+            Rscript code/plot-ancestry-course.R ${scheme}
 
 
