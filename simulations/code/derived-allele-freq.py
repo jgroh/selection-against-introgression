8764e75922feb94543eb70b3e8095795cd368dca
Jeff Groh <brogroh@gmail.com>
2020-10-21 14:09:57 -0700

    new script for calculating allele frequency and update analysis script

---
+++ simulations/code/derived-allele-freq.py
@@ -0,0 +1,31 @@
+import msprime, pyslim
+import numpy as np
+import pandas as pd
+import sys
+import os
+
+# this function computes allele frequency for the derived allele at all segregating sites
+def allele_frequencies(ts, sample_sets=None):
+    if sample_sets is None:
+        sample_sets = [ts.samples()]
+    n = np.array([len(x) for x in sample_sets])
+    def f(x):
+        return x / n
+    return ts.sample_count_stat(sample_sets, f, len(sample_sets), windows='sites', polarised=True, mode='site', strict=False)
+
+
+# Load the .trees file
+ts = pyslim.load(sys.argv[1]).simplify()
+
+# calculate allele frequency for segregating sites and appropriate positions
+val = allele_frequencies(ts).tolist()
+val = [item for sublist in val for item in sublist]
+pos = [int(s.position) for s in ts.sites()] # sites of mutations, should be in same order as reported by the allele frequency calc
+freq = [0]*int(ts.sequence_length) # list that will hold allele frequencies
+
+# fill non-zero values of freq at appropriate positions
+for i in range(0, len(pos)):
+    freq[pos[i]] = val[i]
+
+# output
+print(os.path.basename(sys.argv[1]).rstrip('.trees'), *freq)
--- simulations/code/power-spectrum.R
+++ simulations/code/power-spectrum.R
@@ -22,25 +22,35 @@ ps <- function(x){
 # compute correlation of wavelet coefficients at a single time point
 ####################################################################
 
-wavelet_correlation <- function(data, var1, var2, fam = "DaubExPhase", filt = 1, detail = TRUE){
+wavelet_correlation <- function(data, yvar, xvar, fam = "DaubExPhase", filt = 1, detail = TRUE){
   
-  total.cor <- cor(data[[var1]], data[[var2]], method = "pearson")
-  x.variance <- var(data[[var1]])
-  y.variance <- var(data[[var2]])
+  x <- data[[xvar]]
+  y <- data[[yvar]]
+  total.cor <- cor(x, y, method = "pearson")
   
-  # designed to work for a given time point 
-  x.w <- wd(data[[var1]]-mean(data[[var1]]), family = fam, filter.number = filt)
-  y.w <- wd(data[[var2]]-mean(data[[var2]]), family = fam, filter.number = filt)
+  # calculate empirical variance of each signal (n instead of n-1 in denominator)
+  x.variance <- sum((x-mean(x))^2)/length(x) 
+  y.variance <- sum((y-mean(y))^2)/length(y)
   
+  # wavelet decomposition of each signal
+  x.w <- wd(x, family = fam, filter.number = filt)
+  y.w <- wd(y, family = fam, filter.number = filt)
+  
+  # fill lists while looping over scales of the transformation
   cor.list.detail <- list()
   cor.list.smooth <- list()
-  alpha.x <- list()
-  alpha.y <- list()
+  alpha.x.d <- list()
+  alpha.y.d <- list()
+  alpha.x.s <- list()
+  alpha.y.s <- list()
+  weight <- list()
+  p <- list()
   
-  for(i in 1:x.w$nlevels){
+  for(i in 1:(x.w$nlevels)){
     
     # extract wavelet coefficients at specified level
     
+    # if(i < (x.w$nlevels+1)){
     # detail coefficients
     x.i.d <- accessD(x.w, level = (i-1))
     y.i.d <- accessD(y.w, level = (i-1))
@@ -51,20 +61,45 @@ wavelet_correlation <- function(data, var1, var2, fam = "DaubExPhase", filt = 1,
     y.i.s <- accessC(y.w, level = (i-1))
     cor.list.smooth[[i]]  <- cor(x.i.s, y.i.s, method = "pearson")
       
-    # calculate proportion of variance explained by each scale from detail coefficients
-    alpha.x[[i]] <- sum(x.i.d^2)
-    alpha.y[[i]] <- sum(y.i.d^2)
+    # calculate *proportion* of variance explained by each scale from detail coefficients
+    alpha.x[[i]] <- (sum(x.i.d^2)/length(x)) /x.variance
+    alpha.y[[i]] <- (sum(y.i.d^2)/length(y)) /y.variance
+    
+    weight[[i]] <- sqrt(alpha.x[[i]]*alpha.y[[i]])
+    
+    # calculate weight
+    # fit linear model with intercept forced through origin, obtain r squared
+    s.lm <- summary(lm(y.i.d ~ x.i.d -1))
+    #prp.expl[[i]] <- s.lm$adj.r.squared
+    #p[[i]] <- cor(x.i.d, y.i.d)
+    
+    p[[i]] <- (x.i.d %*% y.i.d)/sqrt(sum(x.i.d^2)*sum(y.i.d^2))
+    
+    # } else{
+    #   cor.list.detail[[i]] <- NA
+    #   cor.list.smooth[[i]] <- NA
+    #   alpha.x[[i]] <- x.variance
+    #   alpha.y[[i]] <- y.variance
+    #   s.lm <- summary(lm(y ~ x - 1))
+    #   weight[[i]] <- s.lm$r.squared
+    #   
+    # }
+    
+    #alpha.x.s[[i]] <- (sum(x.i.s^2)/length(x))/x.variance
+    #alpha.y.s[[i]] <- (sum(y.i.s^2)/length(y))/y.variance
     
   }
-  alpha.x <- unlist(alpha.x)/sum(unlist(alpha.x))
-  alpha.y <- unlist(alpha.y)/sum(unlist(alpha.y))
   
-  names(cor.list.detail) <- paste0("detail-cor:scale",1:x.w$nlevels)
-  names(cor.list.smooth) <- paste0("smooth-cor:scale",1:x.w$nlevels)
   
-  names(alpha.x) <- paste0("alpha-", var1, ":scale", 1:x.w$nlevels)
-  names(alpha.y) <- paste0("alpha-", var2, ":scale", 1:y.w$nlevels)
-  return(c(unlist(cor.list.detail), unlist(cor.list.smooth), alpha.x, alpha.y,total.cor = total.cor))
+  names(cor.list.detail) <- paste0("detail-cor:scale",1:(x.w$nlevels))
+  names(cor.list.smooth) <- paste0("smooth-cor:scale",1:(x.w$nlevels))
+  
+  names(alpha.x) <- paste0("alpha-", var1, ":scale", 1:(x.w$nlevels))
+  names(alpha.y) <- paste0("alpha-", var2, ":scale", 1:(y.w$nlevels))
+  
+  names(weight) <- paste0("weight:scale", 1:(x.w$nlevels))
+  names(p) <- paste0("p:scale", 1:(x.w$nlevels))
+  return(c(unlist(cor.list.detail), unlist(cor.list.smooth), unlist(alpha.x), unlist(alpha.y), total.cor = total.cor, unlist(weight), unlist(p)))
 }
 
 
@@ -73,33 +108,33 @@ wavelet_correlation <- function(data, var1, var2, fam = "DaubExPhase", filt = 1,
 # read and format input data
 ####################################################################
 
-file1 <- "simulations/results/neutral-no-recomb-var/ancestry_master.txt"
-file2 <- "simulations/results/sel-no-recomb-var/ancestry_master.txt"
-file3 <- "simulations/results/neutral-recomb-var/ancestry_master.txt"
-file4 <- "simulations/results/sel-recomb-var/ancestry_master.txt"
+file1 <- "simulations/results/neutral-const-recomb/ancestry_master.txt"
+file2 <- "simulations/results/neutral-periodic-recomb/ancestry_master.txt"
+file3 <- "simulations/results/sel-const-recomb/ancestry_master.txt"
+file4 <- "simulations/results/sel-periodic-recomb/ancestry_master.txt"
 
 
 a1 <- read.table(file1, row.names = 1)
-rownames(a1) <- paste0("neutral-no-recomb-var_", rownames(a1))
+rownames(a1) <- paste0("neutral-const-recomb_", rownames(a1))
 
 a2 <- read.table(file2, row.names = 1)
-rownames(a2) <- paste0("sel-no-recomb-var_", rownames(a2))
+rownames(a2) <- paste0("neutral-periodic-recomb_", rownames(a2))
 
 a3 <- read.table(file3, row.names = 1)
-rownames(a3) <- paste0("neutral-recomb-var_", rownames(a3))
+rownames(a3) <- paste0("sel-const-recomb_", rownames(a3))
 
 a4 <- read.table(file4, row.names = 1)
-rownames(a4) <- paste0("sel-recomb-var_", rownames(a4))
+rownames(a4) <- paste0("sel-periodic-recomb_", rownames(a4))
 
 
 # combine data sets from simulations
-all.sim <- rbind.data.frame(a2,a4)
+all.sim <- rbind.data.frame(a1,a2,a3,a4)
 
 # reformat data for calculation and plotting
 a <- as.data.frame(t(all.sim))
 a$pos_absolute <- 1:1024
-a$pos_on_chrom <- rep(1:64, 16)
-a$chrom <- rep(1:16, each=64)
+# a$pos_on_chrom <- rep(1:64, 16)
+# a$chrom <- rep(1:16, each=64)
 
 # create vector that describes recombination landscape (only applies to sims 3, 4)
 x <- 1:1024
@@ -119,14 +154,14 @@ a$recomb <- r
 
 b  <-  a %>%
   gather(key = sim_rep_gen,
-         value = freq, -c(pos_absolute,pos_on_chrom,chrom,recomb)) %>% 
+         value = freq, -c(pos_absolute,recomb)) %>% 
   separate(sim_rep_gen, c("sim_rep.id", "gen"), sep = "_gen") %>% 
   separate(sim_rep.id, c("sim", "rep.id"), sep = "_replicate")
 
 
 # calcualte average introgressed frequency per simulation per generation
 d  <- b %>% 
-   group_by(sim,pos_absolute,chrom,gen,pos_on_chrom,recomb) %>% 
+   group_by(sim,pos_absolute,gen,recomb) %>% 
    dplyr::summarise(avg.frq = mean(freq))
 
 
@@ -136,18 +171,18 @@ d  <- b %>%
 
 
 # look at frequency in a few snapshots to get a sense for the data
-# b %>% 
-#   filter(sim == "neutral-no-recomb-var", gen == "0002") %>% 
-#   ggplot(aes(x = pos_absolute, y = freq)) + 
+# b %>%
+#   filter(sim == "neutral-no-recomb-var", gen == "0002") %>%
+#   ggplot(aes(x = pos_absolute, y = freq)) +
 #   geom_point() + facet_wrap(~rep.id)
-# 
+#
 # # There is an issue with how ancestry is calcualated - something about how fixed mutations are counted?
-# doesn't make sense why frequency should be >.5 in all replicates in generation 2, and also to decay over time. 
+# doesn't make sense why frequency should be >.5 in all replicates in generation 2, and also to decay over time.
 
 
 # plot average frequency over time, excluding neutral simulations for now
 d %>% 
-  filter(sim == "sel-recomb-var", gen != "0001") %>% 
+  filter(str_detect(sim, "sel")) %>% 
   ggplot(aes(x=pos_absolute, y=avg.frq, group = sim, col = sim)) +
   geom_point() + 
   facet_wrap(~gen, scales = "free_y")
@@ -155,7 +190,7 @@ d %>%
 
 
 cor_text = d %>% 
-  filter(sim == "sel-recomb-var", gen != "0001") %>% 
+  filter(str_detect(sim, "sel-periodic")) %>% 
   group_by(sim,gen) %>% 
   summarise(correlation = round(cor.test(avg.frq,recomb)$estimate, 3) ) %>% #,
   #p.value = round(cor.test(freq,recomb)$p.value, 6)) %>% 
@@ -163,7 +198,7 @@ cor_text = d %>%
 
 
 p <- d %>%
-  filter(sim == "sel-recomb-var", gen != "0001") %>% 
+  filter(str_detect(sim, "sel-periodic")) %>% 
   ggplot(aes(recomb, avg.frq)) +
   geom_point() +
   geom_smooth(method = "lm") +
@@ -211,33 +246,76 @@ frq_ps %>%
 
 # compute correlations using average allele frequency
 wav_cor <- d %>% 
-  filter(sim == "sel-recomb-var", gen != "0001") %>% 
-  ddply("gen", wavelet_correlation, var1 = "recomb", var2 = "avg.frq") %>% 
+  filter(sim == "sel-periodic-recomb") %>% 
+  ddply("gen", wavelet_correlation, xvar = "recomb", yvar = "avg.frq") %>% 
   select_if(~sum(!is.na(.)) > 0) %>% 
   gather(key = temp, value = temp_val, -c(gen,total.cor)) %>% 
   separate(col = temp, into = c("thing", "scale"), sep = ":scale") %>% 
   spread(thing, temp_val) %>%
-  mutate(weighted.cor = ((`alpha-avg.frq`*`alpha-recomb`)^0.5)*`detail-cor`)
+  mutate(weighted.cor = `weight`*`p`)
+
+wav_cor <- wav_cor %>% 
+  group_by(gen) %>% 
+  mutate(sum.weighted.cor = sum(weighted.cor), sum.abs.weighted.cor = sum(abs(weighted.cor)))
+
+
   
 wav_cor$scale <- factor(wav_cor$scale, levels = as.character(1:10))
 
-bars <- wav_cor %>%
-  ggplot(aes(fill = scale, x = gen, y = weighted.cor)) + 
-  geom_bar(position = "stack", stat = "identity", color = "black") +
-  scale_fill_brewer(palette="Dark2")
 
+# plot correlation of detail coefficients 
+wav_cor %>% 
+  ggplot(aes(x=gen, y=`detail-cor`,group=scale,color=scale)) + 
+  geom_point() + 
+  geom_line() + 
+  theme(aspect.ratio = 1) 
 
-total <- wav_cor %>% 
+# plot correlation of smooth coefficients
+wav_cor %>% 
+  ggplot(aes(x=gen, y=`smooth-cor`,group=scale,color=scale)) + 
+  geom_point() + 
+  geom_line() + 
+  theme(aspect.ratio = 1) 
+
+ggplot(wav_cor) +
+  geom_bar(aes(fill = scale, x = gen, y = weighted.cor), position = "stack", stat = "identity", color = "black") +
+  scale_fill_viridis_d(option = "plasma") +
+  geom_point(aes(x = gen, y = total.cor), size=4) 
+  #geom_line(aes(x = gen, y = total.cor, group = 1)) +
+  #geom_line(aes(x = gen,y = sum.weighted.cor, group = 1)) # this line confirms that the sum of weighted correlations equals the total correlation
+  
+
+# normalize by total correlation so that each generation sums to 1
+
+wav_cor <- wav_cor %>% 
   group_by(gen) %>% 
-  summarise(tot = sum(weighted.cor, na.rm = T)) %>% 
-  ggplot(aes(x = gen, y = tot, group = 1)) + 
-  geom_line()
+  mutate(normalized.weighted.cor = abs(weighted.cor)/sum.abs.weighted.cor)
 
-total
+
+wav_cor %>% 
+  ggplot() +
+  geom_bar(aes(fill = scale, x = gen, y = normalized.weighted.cor), 
+           position = "stack", stat = "identity", color = "black") +
+  scale_fill_viridis_d(option = "plasma") 
+  
+  
+
+
+
+
+
+
 
 ggplot(wav_cor, aes(x = gen, y = total.cor, group = 1)) + geom_line()
 
+ggplot(wav_cor, aes(x = gen, y = total.cor, group = 1)) + geom_line()
 
+wav_cor %>% 
+  group_by(gen) %>% 
+  summarise(weighted.sum = sum(weighted.cor, na.rm=T)) %>% 
+  ggplot(aes(x = gen, y = weighted.sum, group = 1)) + geom_point() + geom_line()
+
+total
 
 smooth_cor <- d %>% 
   filter(sim == "sel-recomb-var") %>% 
@@ -264,31 +342,3 @@ smooth_cor %>%
 # compute correlation averaging over wavelet coefficients? ...
 
 
-# decompose correlation
-
-head(detail_cor)
-
-
-wr  <- wd(x$recomb, family = "DaubExPhase", filter.number = 1)
-wf <- wd(x$avg.frq, family = "DaubExPhase", filter.number = 1)
-
-n <- 1024
-s <- vector()
-for(i in 1:wr$nlevels){
-  wx <- accessD(wr, level = i-1)
-  wy <- accessD(wf, level = i-1)
-  s[i] <- wx %*% wy
-}
-
-sum(s)
-
-
-
-
-
-
-
-
-d <- data.frame(temp = c("smooth:scale1", "detail:scale2", "alpha-avg.var1:scale3","alpha-var2:scale4"))
-d
-separate(d, col = temp, into = c("thing", "scale"), sep = ":")
