ce5d4fad27753b7ab98e50a5f862d30e6662b9b1
Jeff Groh <brogroh@gmail.com>
2021-03-01 15:52:04 -0800

    interpolation on physical scale

--- datasets/swordtail_TOTO_ACUA/plot-ACUA-WV.R
+++ datasets/swordtail_TOTO_ACUA/plot-ACUA-WV.R
@@ -14,7 +14,30 @@ allWV <- rbindlist(list(acua2006,acua2008,acua2013,acua2015,acua2018))
 # take average over chromosomes
 wvGnomWide <- allWV[, mean(variance, na.rm=T), by = .(decomp,year,scale)]
 setnames(wvGnomWide, "V1", "variance")
-wvGnomWide %>% ggplot(aes(x = scale, y = variance, group = interaction(decomp, year), color = year)) + 
-  geom_line(aes(linetype=decomp), size=1) + 
-  labs(x = "Scale log2 (N x Morgans)", y = "Wavelet variance")
-  
+wvGnomWide[,propVar := variance/sum(variance),by=.(decomp,year)]
+
+# plot this mf
+wvGnomWide[decomp == "mean_individual"] %>% ggplot(aes(x = scale, y = variance, group = interaction(decomp, year), color = year)) +
+  geom_point() +
+  geom_line(aes(linetype=decomp), size=0.5) + 
+  labs(x = expression(Scale: log[2](Morgans)), 
+       y = "Wavelet variance",
+       color = "Year", linetype = "Signal") +
+  scale_x_discrete(labels = -14:0) + 
+  scale_linetype_discrete(labels = c("Individual","Pop mean"))+
+  theme_classic() +
+  scale_colour_viridis_d()
+
+# plot proportion
+wvGnomWide %>% ggplot(aes(x = scale, y = propVar, group = interaction(decomp, year), color = year)) +
+  geom_point(size=1.8) +
+  geom_line(aes(linetype=decomp), size=0.5) + 
+  labs(x = expression(Scale: log[2](Morgans)), 
+       y = "Proportion of variance",
+       color = "Year", linetype = "Signal") +
+  scale_x_discrete(labels = -14:0) + 
+  scale_linetype_discrete(labels = c("Individual","Pop mean"))+
+  theme_classic() +
+  scale_colour_viridis_d()
+
+
--- datasets/swordtail_TOTO_ACUA/swordtail_wrangle.R
+++ datasets/swordtail_TOTO_ACUA/swordtail_wrangle.R
@@ -35,64 +35,66 @@ chrom1 <- dcast(chrom1, ... ~ genotype, value.var = "post_prob")
 # add column for frequency of allele 1 in individual weighted by post. probs. 
 chrom1[, frq := (gen11 + 0.5*(1 - sum(gen11, gen22))), by = .(ID,chr,phys_pos)]
 
-# Interpolate ancestry in individuals -------------------------
 
-# interpolate ancestry at each SNP position
-chrom1[, c("ind_frq_interp") := 
+# Impute ancestry in individuals -------------------------
+
+# impute ancestry at each SNP position
+chrom1[, c("ind_frq") := 
          approx(x = phys_pos, y = frq, xout = phys_pos, rule = 2)$y, 
        by = .(ID,chr)]
 
 # average over individuals to get population mean
-chrom1[, "pop_mean" := mean(ind_frq_interp), by = .(chr, phys_pos)]
+chrom1[, "pop_mean" := mean(ind_frq), by = .(chr, phys_pos)]
 
 # examine ancestry pattern for a couple of individuals
 #a[V1 %in% unique(a$V1)[1:2]] %>% ggplot(aes(x = phys_pos, y = frq, group = V1, color = V1)) + geom_point()
 
-# Recombination map -----------------------
+# Create recombination map -----------------------
 
 # generate vector of genetic distance using recombination LD map
 # propogate mean values for intervals to all intervening bps 
-r.chrom1.vec <- r.chrom1[, .(rate = rep(mean, times = (right_bp - left_bp ))), by = right_bp][, -c("right_bp")]
+r.chrom1.allbp <- r.chrom1[, .(rho = rep(mean, times = (right_bp - left_bp ))), by = right_bp][, -c("right_bp")]
 
 # extend per bp values to ends of chromosome
 if(min(r.chrom1$left_bp) > 1){
-  front <- data.table(rate = rep(r.chrom1[left_bp == min(left_bp), mean], times=min(r.chrom1$left_bp)))
+  front <- data.table(rho = rep(r.chrom1[left_bp == min(left_bp), mean], times=min(r.chrom1$left_bp)))
 } else{front <- data.table()}
 if(max(r.chrom1$right_bp) < max(chrom1$phys_pos)){
   back <- data.table(
-    rate = rep(r.chrom1[right_bp == max(right_bp), mean],
+    rho = rep(r.chrom1[right_bp == max(right_bp), mean],
                times=(max(chrom1$phys_pos)-max(r.chrom1$right_bp))))
 } else{back <- data.table()}
 
 # stick together for full rec. map per bp 
-r.chrom1.vec <- rbindlist(list(front, r.chrom1.vec, back))
+r.chrom1.allbp <- rbindlist(list(front, r.chrom1.allbp, back))
+
+# get genetic distance, divide out 2Ne
+Ne2 <- 97739 # see script scaff_lengths.R which compares LD map lengths to cM map lengths
+
+r.chrom1.allbp[, r := rho/Ne2] # rho estimated by LDHelmet is 2Ner
+#r.chrom1.allbp[, mb_per_cM := 1/(r*1e2*1e6)]
+r.chrom1.allbp[, Morgan := cumsum(r)]
 
 # assign genetic position of SNPS present in the data and per bp rate
-chrom1[, c("gen_pos_rho", "rec_rate") := 
-         list(cumsum(r.chrom1.vec$rate)[phys_pos], r.chrom1.vec$rate[phys_pos]),
+chrom1[, c("Morgan", "r") := 
+         list(r.chrom1.allbp[,Morgan][phys_pos], r.chrom1.allbp[,r][phys_pos]),
        by = .(chr)]
 
-# we divide out Ne from genetic distances 
-Ne2 <- 97739 # see script scaff_lengths.R which compares LD map lengths to cM map lengths
-chrom1[, "gen_pos_M" := gen_pos_rho/Ne2]
-
-# sanity checks
+# sanity check
 # dim(r.chrom1.vec)[1] == max(chrom1$phys_pos)
-# plot gen distancew against physical
+# plot gen distance against physical
 # ind <- chrom1[ID == ID[1]]$phys_pos
 # plot(y = cumsum(r.chrom1.vec$rate)[ind], x = chrom1[ID == ID[1]]$phys_pos)
-
 
 # plot population mean of minor parent ancestry along chromosome
 #chrom1[ID == ID[1]] %>% ggplot(aes(x = phys_pos, y = 1-pop_mean)) + geom_point()
 
-# plot overall correlation between rec. and population mean ancestry on per bp level
-# chrom1[ID == ID[1], .(pop_mean, rec_rate)] %>% 
-#   ggplot(aes(x = log(rec_rate), y = 1-pop_mean)) + 
-#   geom_point() + geom_smooth(method = "lm") + 
-#   labs(x = "log(rho / bp)", y = "Minor parent ancestry")
+# vs. genetic distance (non-interpolated genetic distances)
+#chrom1[ID == ID[1]] %>% ggplot(aes(y = 1-pop_mean, x = gen_pos)) + 
+#  geom_point() + labs(x = "Genetic distance (2N Morgans)", 
+#                      y = "Avg. minor parent ancestry")
 
-# chrom1[ID == ID[1], cor(rec_rate, 1-pop_mean)]
+
 
 # Average rec. and ancestry in 50 kb windows -----------------
 
@@ -118,41 +120,132 @@ chrom1[, "gen_pos_M" := gen_pos_rho/Ne2]
 #               size=3, color="red") 
 
 
-# plot minor parent ancestry vs physical distance
-#chrom1[ID == ID[1]] %>% ggplot(aes(y = 1-pop_mean, x = phys_pos))+geom_point()
-
-# vs. genetic distance (non-interpolated genetic distances)
-#chrom1[ID == ID[1]] %>% ggplot(aes(y = 1-pop_mean, x = gen_pos)) + 
-#  geom_point() + labs(x = "Genetic distance (4N Morgans)", 
-#                      y = "Avg. minor parent ancestry")
-
-
 # Interpolate at evenly spaced genetic distances -------
 
 # what unit to interpolate at?
 # check # of SNPs / Morgan:
-length(unique(chrom1$phys_pos))/max(chrom1$gen_pos_M) #roughly 50000 SNPs per M across chromosomes
+#length(unique(chrom1$phys_pos))/max(chrom1$Morgan) #roughly 50000 SNPs per M across chromosomes
 # so we can interpolate to unit distance such that we get approx 1 SNP per unit
 # this distance is roughly M*2^-15
 
-xout = seq(0, max(chrom1$gen_pos_M), by=2^-15) 
+xout = seq(0, max(chrom1$Morgan), by=2^-15) 
 
 # interpolate individual ancestry at genetic coordinates
-chrom1.interp <- chrom1[, approx(x = gen_pos_M, 
-                                 y = ind_frq_interp, 
+chrom1.interp.gen <- chrom1[, approx(x = Morgan, 
+                                 y = ind_frq, 
                                  xout = xout, rule = 2), 
                         by = .(ID,chr)]
-setnames(chrom1.interp, c("x", "y"), c("gen_pos_M","ind_frq_interp"))
+setnames(chrom1.interp.gen, c("x", "y"), c("Morgan","ind_frq_interp"))
 
 # compute population mean
-chrom1.interp[, pop_mean_interp := mean(ind_frq_interp), 
-              by = .(chr, gen_pos_M)]
+chrom1.interp.gen[, pop_mean_interp := mean(ind_frq_interp), 
+              by = .(chr, Morgan)]
+
+
+# Interpolate on physical scale for correlation analysis --------
+
+# approximately 1 SNP per kb so interpolate at this resolution
+
+xoutbp = seq(0, max(chrom1$phys_pos), by=1e3) 
+
+chrom1.interp.phys <- chrom1[, approx(x = phys_pos, 
+                                 y = ind_frq, 
+                                 xout = xoutbp, rule = 2), 
+                        by = .(ID,chr)]
+setnames(chrom1.interp.phys, c("x", "y"), c("position","ind_frq_interp"))
+
+# compute population mean
+chrom1.interp.phys[, pop_mean_interp := mean(ind_frq_interp), 
+              by = .(chr, position)]
+
+# interpolate recombination at same resolution
+r.interp.phys <- chrom1[, approx(x = phys_pos, 
+                y = r, 
+                xout = xoutbp, rule = 2), by = .(ID,chr)]
+setnames(r.interp.phys, c("x", "y"), c("position","r_interp"))
+
+# combine these
+chrom1.interp.phys <- merge(r.interp.phys,chrom1.interp.phys, by = c("ID","chr","position"))
+
+# look at SNP density along genome
+# u <- chrom1[,seq(min(phys_pos),max(phys_pos),length.out=512)]
+# v <- density(unique(chrom1$phys_pos))$y
+# plot(y=v, x = u)
+
+# compare correlation at original data points to interpolated locations
+# ggplot(chrom1[ID==ID[1]], aes(x = log(r) , y = 1-pop_mean)) + geom_point() + geom_smooth(method = "lm")
+# cor.test(chrom1[ID==ID[1],log(r)], chrom1[ID==ID[1], 1-pop_mean])
+# 
+# ggplot(chrom1.interp.phys[ID==ID[1]], aes(x = log(r_interp),y = 1-pop_mean_interp)) + 
+#    geom_point() + geom_smooth(method = "lm")
+# cor.test(chrom1.interp.phys[ID==ID[1],log(r_interp)], chrom1.interp.phys[ID==ID[1],1-pop_mean_interp])
+
+# output
+save(chrom1, chrom1.interp.gen, chrom1.interp.phys, file = paste0("ACUA_",year,"/",scaff,".RData"))
+
+
+# Examine effect of interpolation on genetic scale to correlation -----------
+
+# intepolate rec map to get rec values at corresponding M distances
+# r.chrom1.interp <- r.chrom1.allbp[, approx(x = Morgan, 
+#                                  y = r, 
+#                                  xout = xout, rule = 2)]
+
+# setnames(r.chrom1.interp, c("x", "y"), c("Morgan","r"))
+
+# add this to chrom1 interp
+# chrom1.interp[,r := r.chrom1.interp$r, by = .(ID,chr)]
+
+# ggplot(chrom1[ID==ID[1]], aes(x = log(r) , y = 1-pop_mean)) + geom_point() + geom_smooth(method = "lm")
+# cor.test(chrom1[ID==ID[1],log(r)], chrom1[ID==ID[1], 1-pop_mean])
+
+# ggplot(chrom1.interp[ID==ID[1]], aes(x = log(r),y = 1-pop_mean_interp)) + 
+#   geom_point() + geom_smooth(method = "lm")
+# cor.test(chrom1.interp[ID==ID[1],log(r)], chrom1.interp[ID==ID[1],1-pop_mean_interp])
+
+# the correlation appears much weaker
+
+# # Interpolation method #2 recombination rates 
+# # for each SNP position, get cM per kb comparing to previous SNP
+# cmPerKb <- function(x){
+#   if ( x[,phys_pos] == min(chrom1[,phys_pos]) ){
+#     lowerSNP <- 0
+#     lowerM <- 0
+#   } else{
+#     lowerSNP <- max(chrom1[phys_pos < x[,phys_pos], phys_pos])
+#     lowerM <- chrom1[phys_pos == lowerSNP, Morgan][1]
+#   }
+#   return(100*1000*(x[,phys_pos] - lowerSNP)/(x[,Morgan] - lowerM))
+# }
+
+# add identifier number to each snp
+# chrom1[, num := seq_len(nrow(.SD)), by=ID]
+# chrom1[ID==ID[1], cmPerKb(.SD),by=num]
+
+
+# # Interpolation method #3 recombination rates 
+# getRecRate <- function(x){
+#   # x should be the interpolated genetic distance in chrom1.interp
+#   # get the next smallest M distance and bp for real SNP locations 
+#   if (min(chrom1[,Morgan]) < x[,Morgan]){
+#     maxLess_M <- max(chrom1[Morgan < x[,Morgan], Morgan])
+#     maxLess_Bp <- chrom1[Morgan == maxLess_M, phys_pos][1] 
+#   } else{ 
+#     maxLess_M <- 0
+#     maxLess_Bp <- 0
+#   }
+#   
+#   # next largest value of genetic distance
+#   minGreater_M <- min(chrom1[Morgan >= x[,Morgan], Morgan])
+#   minGreater_Bp <- chrom1[Morgan == minGreater_M, phys_pos][1] 
+#   
+#   # multiply by 1000 and 100 to get cM/kb from M/bp
+#   return(1000*100*(minGreater_M-maxLess_M)/(minGreater_Bp-maxLess_Bp))
+# }
+#   
+
 
 #chrom1.interp[ID == ID[1]] %>% ggplot(aes(x = gen_pos, y = 1-pop_mean_interp)) + geom_point()
 
-save(chrom1, chrom1.interp, file = paste0("ACUA_",year,"/",scaff,".RData"))
-#load(file="datasets/schumer_etal_2019/TOTO_chrom1.Rdata")
 
-## End Formatting
-#head(chrom1.interp)
 
