f4a28b69fb62e8aab86c7b372c13db60083c3e15
Jeff Groh <brogroh@gmail.com>
2021-03-04 15:36:29 -0800

    wvss_params

--- simulations/code/wvss_params.R
+++ simulations/code/wvss_params.R
@@ -1,15 +1,11 @@
-
-
-
 params <- NULL
-for(g in c(5,10,50,100,500,1000)){
+for(g in c(1000)){
   for(i in 1:10){
-   params <- c(params,paste(g,i,1:(2^((11-i)-1))))
+   params <- c(params,paste(g,i,1:(2^((11-i)-1)),sep = "_"))
   }
 }
-tail(params)
 pars <- as.matrix(params)
 write.table(pars,
-            file = "~/workspace/selection-against-introgression/simulations/code/wv_batch_params.txt",
+            file = "~/workspace/selection-against-introgression/simulations/code/wvss_params.txt",
             quote = F,row.names = F,col.names=F)
 
