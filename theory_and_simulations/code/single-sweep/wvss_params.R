3e7f0eb6c059e2fce751cc44e10be3a21165ddb5
Jeff Groh <brogroh@gmail.com>
2021-03-09 10:05:39 -0800

    add sel coeff to wvss_params

--- theory_and_simulations/code/single-sweep/wvss_params.R
+++ theory_and_simulations/code/single-sweep/wvss_params.R
@@ -3,14 +3,16 @@ library(data.table)
 params <- NULL
 for(g in c(5,10,50,100,500,1000)){
   for(i in 1:10){
-   params <- c(params,paste(g,i,1:(2^((11-i)-1)),sep = "_"))
+    for(s in c(0.01,0.1,0.5)){
+   params <- c(params,paste(g,i,1:(2^((11-i)-1)),s,sep = "_"))
+    }
   }
 }
 pars <- params %>% as.data.table() %>%
-  separate(1, into = c("gen","scale","shift"), sep = "_") %>%
+  separate(1, into = c("gen","scale","shift","s"), sep = "_") %>%
   .[, lapply(.SD, as.numeric)]
 
-# use sparse wavelets to approximate the integral for speed here
+# use sparse wavelets to approximate the avg. over all wavelet placements at a scale
 spars <- rbindlist(list(pars[scale %in% 8:10 & shift %% 2 != 0,],
                         pars[scale %in% 6:7 & shift %% 4 == 0,],
                         pars[scale %in% 4:5 & shift %% 8 == 0,],
@@ -18,7 +20,7 @@ spars <- rbindlist(list(pars[scale %in% 8:10 & shift %% 2 != 0,],
                         pars[scale ==1 & shift %% 64 == 0,]
                         ))
 
-spars.out <- as.matrix(spars[,paste(gen,scale,shift, sep = "_")])
+spars.out <- as.matrix(spars[,paste(gen,scale,shift,s, sep = "_")])
 write.table(spars.out,
             file = "wvss_params.txt",
             quote = F,row.names = F,col.names=F)
