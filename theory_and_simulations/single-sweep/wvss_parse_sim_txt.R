b9cfaef47559edb86ae38fbb561c982ce4760081
Jeff Groh <brogroh@gmail.com>
2021-03-23 18:02:17 -0400

    update to wav-var-pop-model.R with more sample sizes and add wv of pop mean to wvss_parse_sim_txt.R

--- theory_and_simulations/single-sweep/wvss_parse_sim_txt.R
+++ theory_and_simulations/single-sweep/wvss_parse_sim_txt.R
@@ -22,15 +22,57 @@ f[,"position" := 1:1024]
 f <- melt(f, id.vars = "position", variable.name = "id", 
      value.name = "allele")
 
-# wavelet variance by individual
-mwt <- f[,wave.variance(brick.wall(modwt(allele, "haar", n.levels = 10),"haar")), by = id]
-mwt[, scale:= 1:11, by= id]
-mwt <- mwt[scale != 11]
+# look at allele frequency
+# f[, mean(allele), by = position] %>% 
+#   ggplot(aes(x = position, y = V1)) + geom_point()
 
-avg.wv <- mwt[, mean(wavevar), by = scale]
-setnames(avg.wv, "V1", "wavevar")
-avg.wv[, s := s]
-avg.wv[, l.s := l.s]
-avg.wv[, gen := gen]
+# wavelet coefficients for individuals
+mwtInd <- f[,brick.wall(wf="haar",x=modwt(allele, "haar", n.levels = 10)), by = id]
+mwtInd[, position := 1:1024, by = id]
+mwtAvgInd <- mwtInd[, mean(d9), by = position] 
+setnames(mwtAvgInd, "V1", "d9")
 
-save("avg.wv", file = paste0("sims/",sim,".RData"))
+# wavelet coefficidnts for population mean
+frq <- f[, mean(allele), by=position]
+setnames(frq, "V1", "frequency")
+mwtMean <- frq[,brick.wall(wf="haar",x=modwt(frequency, "haar", n.levels = 10))]
+mwtMean <- data.table(mwtMean$d9)
+mwtMean[, position := 1:1024]
+setnames(mwtMean, "V1", "d9")
+
+# plot scale 9 wavelet coefficients
+# mwtInd[id %in% unique(id)[1:20]] %>% ggplot(aes(x = position, y = d9)) +
+#   geom_line(aes(group = id, color = id)) +
+#   theme(legend.position = "none") +
+#   geom_line(data=mwtAvgInd, size = 1.5, alpha = 0.8) +
+#   labs(y = "Scale 9 coefficients")
+
+# wavelet variance by individual haplotype
+wvInd <- f[,wave.variance(brick.wall(modwt(allele, "haar", n.levels = 10),"haar")), by = id]
+wvInd[, scale:= 1:11, by= id]
+wvInd <- wvInd[scale != 11] # remove large scale smooth 
+wvInd[, signal := "mean_ind"]
+
+# average over haplotypes
+wvAvgInd <- wvInd[, mean(wavevar), by = scale]
+setnames(wvAvgInd, "V1", "wavevar")
+wvAvgInd[,signal := "mean_ind"][, s := s][, l.s := l.s][, gen := gen][]
+
+# wavelet variance for population mean
+wvPopMean <- as.data.table(frq[,wave.variance(brick.wall(modwt(frequency, "haar", n.levels = 10),"haar"))])
+wvPopMean[, scale:= 1:11]
+wvPopMean <- wvPopMean[scale != 11] # remove large scale smooth 
+wvPopMean[, lower:=NULL]
+wvPopMean[, upper:=NULL]
+wvPopMean[,signal := "pop_mean"]
+wvPopMean[, s := s][, l.s := l.s][, gen := gen][]
+
+# output 
+simWV <- merge(wvPopMean, wvAvgInd, all = T)
+
+save(simWV, file = paste0("sims/",sim,".RData"))
+
+# plot wavelet spectrum
+#simWV[signal == "pop_mean"] %>% 
+#  ggplot(aes(x = scale, y = wavevar, group = signal)) + 
+#  geom_point() + geom_line() + theme_classic()
