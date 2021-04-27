5a2e8f4cb0a476239c03e439a042e93c616c44e2
Jeff Groh <brogroh@gmail.com>
2021-04-27 11:47:30 -0700

    minor change to allChr_1kb_bed.R

--- datasets/swordtail_TOTO_ACUA/allChr_1kb_bed.R
+++ datasets/swordtail_TOTO_ACUA/allChr_1kb_bed.R
@@ -13,10 +13,13 @@ setnames(a, c("scaff", "len"))
 scaffLengths <- a[, as.numeric(max(len)), by=scaff]
 setnames(scaffLengths, "V1", "len")
 
-# make bed
+# make bed 
 bed <- scaffLengths[, seq(0, max(len), by=1e3), by = .(scaff,len)]
 setnames(bed, "V1", "midpoint")
 
 bed[, c("start", "end") := .(ifelse(midpoint == 0, 0, midpoint - 500), 
                              ifelse(midpoint == max(midpoint), max(len)+1, midpoint + 500)), by = scaff]
 bed[, c("len","midpoint") := NULL]
+
+# write out
+write.table(bed, file = "allChr_1kb.bed", quote = F, sep = "\t", col.names = F, row.names = F)
