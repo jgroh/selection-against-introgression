f36d0893d769fae01a36888a8aa564cebadccc19
Jeff Groh <jgroh@ucdavis.edu>
2021-05-10 15:12:45 -0700

    rename bed files to be more descriptive, update Snakefile accordingly

--- datasets/swordtail_TOTO_ACUA/merge_cds_and_cm_1kb.R
+++ datasets/swordtail_TOTO_ACUA/merge_cds_and_cm_1kb.R
@@ -1,7 +1,7 @@
 library(data.table)
 
-cds <- fread("cds_per_kb.bed", col.names = c("chr", "start", "end", "coding_bp"))
-cm <- fread("allChr_1kb_rec.bed", col.names = c("chr", "start", "end", "cM"))
+cds <- fread("xbir_1kb_CDS.bed", col.names = c("chr", "start", "end", "coding_bp"))
+cm <- fread("xbir_1kb_rec.bed", col.names = c("chr", "start", "end", "cM"))
 
-write.table(merge(cds,cm), file = "allChr_cds_and_cm_per_1kb.txt", quote=F, sep = "\t", row.names=F,col.names=T)
+write.table(merge(cds,cm), file = "xbir_1kb_CDS_and_cM.txt", quote=F, sep = "\t", row.names=F,col.names=T)
 
