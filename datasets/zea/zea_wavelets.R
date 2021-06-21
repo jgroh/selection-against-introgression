92bb24c46102e16b946c8db6e79f530c13ee66a6
Jeff Groh <brogroh@gmail.com>
2021-06-21 15:52:27 -0700

    add individual decomp to zea_wavelets.R

--- datasets/zea/zea_wavelets.R
+++ datasets/zea/zea_wavelets.R
@@ -1,36 +1,37 @@
+# ===== Inputs =====
+
 library(data.table)
 library(waveslim)
 
+# arguments from command line
 args <- commandArgs(trailingOnly = TRUE)
 metaFile <- args[1]
 pop <- args[2]
 ancPath <- args[3]
 outPath <- args[4]
 
+# run this block only if running locally
+#pop <- "RIMMA0366"
+#ancPath <- "hmm_anc_interp/genetic/"
+#metaFile <- "HILO_MAIZE55_PARV50_meta.txt"
+#
+
+# Read files
 meta <- fread(metaFile)
 
 # read whichever files present from individuals in focal population
 indFiles <- as.list(paste0(ancPath, 
                            intersect(list.files(ancPath), 
                                      paste0(meta[RI_ACCESSION==pop,ID],".txt"))))
-# combine individuals
+# combine individuals from population into one table
 gnoms <- rbindlist(lapply(indFiles, fread))
 
-# logit transform ancestry
-gnoms[freqMex > 1, freqMex := 1] # strangely some values slightly > 1, precision error?
 
-# replace values of zero or 1 by small deviation so logit works
-epsilon <- gnoms[freqMex > 0, min(freqMex)]/2
-gnoms[, freqMexTr := log(freqMex/(1-freqMex))]
-gnoms[freqMex == 0, freqMexTr := log(epsilon/(1-epsilon))]
-gnoms[freqMex == 1, freqMexTr := log((1-epsilon)/epsilon)]
+# ===== Define Functions =====
 
-# take mean over individuals
-meanAnc <- gnoms[, lapply(.SD,mean),.SDcols = c('freqMex','freqMexTr'),by=.(Morgan,chr)]
-
-# To compute modwt on variable in a data table:
+# maximum overlap discrete wavelet transform
 modwtAllScales <- function(x,variable,lenCol,allcols){
-    dt <- setDT( 
+  dt <- setDT( 
     x[,brick.wall(wf="haar", x=modwt(variable-mean(variable),"haar",n.levels=floor(log2(length(unique(lenCol))))))] 
   )
   # remove smooth coeff column 
@@ -43,12 +44,38 @@ modwtAllScales <- function(x,variable,lenCol,allcols){
   return(dt)
 }
 
+# Unbiased estimator of wavelet variance for MODWT with brick wall boundary condition
+wav_var <- function(x){sum(x^2,na.rm=TRUE)/(length(x[!is.na(x)]))} 
+
+
+# ===== Data transformations =====
+
+# logit transform ancestry (?)
+gnoms[freqMex > 1, freqMex := 1] # strangely some values slightly > 1, precision error?
+
+# replace values of zero or 1 by small deviation so logit works
+epsilon <- gnoms[freqMex > 0, min(freqMex)]/2
+gnoms[, freqMexTr := log(freqMex/(1-freqMex))]
+gnoms[freqMex == 0, freqMexTr := log(epsilon/(1-epsilon))]
+gnoms[freqMex == 1, freqMexTr := log((1-epsilon)/epsilon)]
+
+# take mean over individuals
+meanAnc <- gnoms[, lapply(.SD,mean),.SDcols = c('freqMex','freqMexTr'),by=.(Morgan,chr)]
+
+
+# ===== Wavelet Transform =====
+
 # define levels based on longest chromosome
 maxLevel <- max(gnoms[,floor(log2(length(unique(Morgan)))),by=chr][,2])
 allCols <- paste0("d",1:maxLevel)
 
+# MODWT on individuals
+indAncModwt <- gnoms[, modwtAllScales(.SD,variable=freqMexTr,lenCol=Morgan,allcols=allCols),by=.(ID,chr)]
+
 # MODWT on mean ancestry
 meanAncModwt <- meanAnc[,modwtAllScales(.SD,variable=freqMexTr,lenCol=Morgan,allcols=allCols), by = chr]
+
+# ===== Wavelet Variance =====
 
 # number of wavelet coefficients per scale on each chromosome
 chrWeights <- meanAncModwt[,lapply(.SD,function(x)length(x[!is.na(x)]) ), by = chr]
@@ -59,23 +86,41 @@ chrWeights <- melt(chrWeights, id.vars = "chr", measure.vars = as.character(1:ma
 # rescale weights to percentages
 chrWeights[,weight := numCoeffs/sum(numCoeffs), by=.(scale)]
 
-# Unbiased estimator of wavelet variance for MODWT with brick wall boundary condition
-wav_var <- function(x){sum(x^2,na.rm=TRUE)/(length(x[!is.na(x)]))} 
+# ----- wavelet variance: chromosome x individual ----- 
+indAncWavVarChrs <- indAncModwt[,lapply(.SD,wav_var),by=.(chr,ID)]
 
-# wavelet variance by chromosome
+# ----- wavelet variance: individual -----
+# weighted average over chromosomes within each individual
+setnames(indAncWavVarChrs, paste0("d",1:maxLevel), as.character(1:maxLevel))
+indAncWavVarChrs <- melt(indAncWavVarChrs, measure.vars = as.character(1:maxLevel),
+                          variable.name = "scale", value.name = "anc_variance")
+# weighted average over chromosomes
+indAncWavVar <- merge(indAncWavVarChrs, chrWeights) 
+indAncWavVar <- indAncWavVar[, weighted.mean(anc_variance, weight), by=.(ID,scale)]
+setnames(indAncWavVar, "V1", "anc_variance")
+# average over individuals
+indMeanAncWavVar <- indAncWavVar[, mean(anc_variance), by = scale]
+setnames(indMeanAncWavVar, "V1", "anc_variance")
+indMeanAncWavVar[,decomp :="individual"]
+
+
+# ----- wavelet variance: mean -----
 meanAncWavVarChrs <- meanAncModwt[,lapply(.SD,wav_var),by=chr]
 setnames(meanAncWavVarChrs, paste0("d",1:maxLevel), as.character(1:maxLevel))
 meanAncWavVarChrs <- melt(meanAncWavVarChrs, measure.vars = as.character(1:maxLevel),
                        variable.name = "scale", value.name = "anc_variance")
-
-# weighted average of wavelet variance over chromosomes
+# weighted average over chromosomes
 meanAncWavVar <- merge(meanAncWavVarChrs, chrWeights) 
 meanAncWavVar <- meanAncWavVar[, weighted.mean(anc_variance, weight), by = scale]
 setnames(meanAncWavVar, "V1", "anc_variance")
 meanAncWavVar[,decomp :="mean"]
-meanAncWavVar[,popID := pop]
-meanAncWavVar[,species := meta[RI_ACCESSION==pop,zea][1]]
-meanAncWavVar[,locality := meta[RI_ACCESSION==pop,LOCALITY][1]]
+
+# ----- combine tables and output -----
+allAncWavVar <- rbind(meanAncWavVar, indMeanAncWavVar)
+allAncWavVar[,popID := pop]
+allAncWavVar[,species := meta[RI_ACCESSION==pop,zea][1]]
+allAncWavVar[,locality := meta[RI_ACCESSION==pop,LOCALITY][1]]
+
 
 
 save(meanAncWavVar, file=outPath)
