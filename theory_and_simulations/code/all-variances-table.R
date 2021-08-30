96de359c3adeba5392c873809c89953ddf3c4656
Jeff Groh <brogroh@gmail.com>
2021-08-30 12:17:45 -0700

    update equilbrium_n500 and all-variances-table.R should work now in snakemake

--- theory_and_simulations/code/all-variances-table.R
+++ theory_and_simulations/code/all-variances-table.R
@@ -16,7 +16,7 @@ v <- var(rowMeans(a))
 
 # calculate wavelet variances for population mean
 d <- dwt(colMeans(a), "haar", n.levels = 10)
-WV <- data.table(population = sapply(d[-1], function(x)sum(x^2)/1024), level = 1:10)
+WV <- data.table(population_wavVar = sapply(d[-1], function(x)sum(x^2)/1024), level = 1:10)
 
 # calculate wavelet variances for individuals and append to table
 for(i in 1:nrow(a)){
@@ -27,11 +27,11 @@ for(i in 1:nrow(a)){
 }
 
 # average over individuals
-WV[, meanInd  := rowMeans(WV[, !c("level", "population")])]
+WV[, meanInd_wavVar  := rowMeans(WV[, !c("level", "population_wavVar")])]
 
-output <- WV[, c("level", "population", "meanInd")]
+output <- WV[, c("level", "population_wavVar", "meanInd_wavVar")]
 
-output[, var := v]
+output[, indMeanVar := v]
 output[, rep_gen := rep_gen]
 
 write.table(output, file = outfile, quote =F, row.names =F, col.names =T, sep = "\t")
