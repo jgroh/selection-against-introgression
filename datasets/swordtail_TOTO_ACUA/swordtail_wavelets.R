3941b36114fa4c0403e936451a1ba65917103b6e
Jeff Groh <brogroh@gmail.com>
2022-03-08 10:04:16 -0800

    fix errors in swordtail_wavelets

--- datasets/swordtail_TOTO_ACUA/swordtail_wavelets.R
+++ datasets/swordtail_TOTO_ACUA/swordtail_wavelets.R
@@ -121,78 +121,108 @@ wavvar <- rbind(wavvar_G, wavvar_P)
 
 # # ====== Correlation decomp =====
 # ---- calculate correlations between ancestry and both recombination and gene density. since recombination and gene
-# density are themselves correlated, calculate partial correlations
+# density are themselves correlated, also calculate partial correlations
 
 # ----- physical units
 oldnames <- c("cor_n", "cor_jack", "lower95ci", "upper95ci")
 
-# for partial correlation of freq and recombination, regress each against cds and take residuals
-gnomP[ID==ID[1], frq_cds_res := residuals(lm(meanFreq~cds_density))]
-gnomP[ID==ID[1], r_cds_res := residuals(lm(r~cds_density))]
-gnomP[ID==ID[1], log10r_cds_res := residuals(lm(log10r~cds_density))]
-
-cortbl1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_cds_res", "r_cds_res"))]
+cortbl1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("meanFreq", "r"))]
 setnames(cortbl1P, old = oldnames, new = paste0(oldnames, ".meanFreq_r"))
 
-cortbl1.1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_cds_res", "log10r_cds_res"))]
-setnames(cortbl1.1P, old = oldnames, new = paste0(oldnames, ".meanFreq_log10r"))
+cortbl2P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("meanFreq", "cds_density"))]
+setnames(cortbl2P, old = oldnames, new = paste0(oldnames, ".meanFreq_cds"))
 
-# for partial correlation of freq and cds, regress each against recomb
-gnomP[ID==ID[1], frq_r_res := residuals(lm(meanFreq~r))]
-gnomP[ID==ID[1], frq_log10r_res := residuals(lm(meanFreq~log10r))]
-gnomP[ID==ID[1], cds_r_res := residuals(lm(cds_density~r))]
-gnomP[ID==ID[1], cds_log10r_res := residuals(lm(cds_density~log10r))]
-
-cortbl2P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_r_res", "cds_r_res"))]
-setnames(cortbl2P, old = oldnames, new = paste0(oldnames, ".meanFreq_cdsDensity.resid_r"))
-
-cortbl2.1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_log10r_res", "cds_log10r_res"))]
-setnames(cortbl2.1P, old = oldnames, new = paste0(oldnames, ".meanFreq_cdsDensity.resid_log10r"))
-
-# for correlation between recombination and gene density, use straight up correlation
 cortbl3P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("r", "cds_density"))]
-setnames(cortbl3P, old = oldnames, new = paste0(oldnames, ".r_cdsDensity"))
+setnames(cortbl3P, old = oldnames, new = paste0(oldnames, ".r_cds"))
 
-cortbl3.1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("log10r", "cds_density"))]
-setnames(cortbl3.1P, old = oldnames, new = paste0(oldnames, ".log10r_cdsDensity"))
 
-wavcorP <- merge(merge(merge(cortbl1P, cortbl1.1P), merge(cortbl2P, cortbl2.1P)), merge(cortbl3P, cortbl3.1P))
+# 
+# # for partial correlation of freq and recombination, regress each against cds and take residuals
+# gnomP[ID==ID[1], frq_cds_res := residuals(lm(meanFreq~cds_density))]
+# gnomP[ID==ID[1], r_cds_res := residuals(lm(r~cds_density))]
+# gnomP[ID==ID[1], log10r_cds_res := residuals(lm(log10r~cds_density))]
+# 
+# cortbl1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("meanFreq", "r"))]
+# setnames(cortbl1P, old = oldnames, new = paste0(oldnames, ".meanFreq_r"))
+# 
+# cortbl1.1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_cds_res", "r_cds_res"))]
+# setnames(cortbl1.1P, old = oldnames, new = paste0(oldnames, ".meanFreq_r_prtl"))
+# 
+# cortbl1.2P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_cds_res", "log10r_cds_res"))]
+# setnames(cortbl1.2P, old = oldnames, new = paste0(oldnames, ".meanFreq_log10r_prtl"))
+# 
+# # for partial correlation of freq and cds, regress each against recomb
+# gnomP[ID==ID[1], frq_r_res := residuals(lm(meanFreq~r))]
+# gnomP[ID==ID[1], frq_log10r_res := residuals(lm(meanFreq~log10r))]
+# gnomP[ID==ID[1], cds_r_res := residuals(lm(cds_density~r))]
+# gnomP[ID==ID[1], cds_log10r_res := residuals(lm(cds_density~log10r))]
+# 
+# cortbl2P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_r_res", "cds_r_res"))]
+# setnames(cortbl2P, old = oldnames, new = paste0(oldnames, ".meanFreq_cdsDensity.resid_r"))
+# 
+# cortbl2.1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_log10r_res", "cds_log10r_res"))]
+# setnames(cortbl2.1P, old = oldnames, new = paste0(oldnames, ".meanFreq_cdsDensity.resid_log10r"))
+# 
+# # for correlation between recombination and gene density, use straight up correlation
+# cortbl3P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("r", "cds_density"))]
+# setnames(cortbl3P, old = oldnames, new = paste0(oldnames, ".r_cdsDensity"))
+# 
+# cortbl3.1P <- gnomP[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("log10r", "cds_density"))]
+# setnames(cortbl3.1P, old = oldnames, new = paste0(oldnames, ".log10r_cdsDensity"))
+# 
+# wavcorP <- merge(merge(merge(cortbl1P, cortbl1.1P), merge(cortbl2P, cortbl2.1P)), merge(cortbl3P, cortbl3.1P))
+
+wavcorP <- merge( merge(cortbl1P, cortbl2P),  cortbl3P)
 wavcorP[, units := "physical"]
 
 
 
 # ----- genetic units
-# for partial correlation of freq and recombination, regress each against cds and take residuals
-gnomG[ID==ID[1], frq_cds_res := residuals(lm(meanFreq~cds_density))]
-gnomG[ID==ID[1], r_cds_res := residuals(lm(r~cds_density))]
-gnomG[ID==ID[1], log10r_cds_res := residuals(lm(log10r~cds_density))]
 
-cortbl1G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_cds_res", "r_cds_res"))]
+cortbl1G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("meanFreq", "r"))]
 setnames(cortbl1G, old = oldnames, new = paste0(oldnames, ".meanFreq_r"))
 
-cortbl1.1G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_cds_res", "log10r_cds_res"))]
-setnames(cortbl1.1G, old = oldnames, new = paste0(oldnames, ".meanFreq_log10r"))
+cortbl2G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("meanFreq", "cds_density"))]
+setnames(cortbl2G, old = oldnames, new = paste0(oldnames, ".meanFreq_cds"))
 
-# for partial correlation of freq and cds, regress each against recomb
-gnomG[ID==ID[1], frq_r_res := residuals(lm(meanFreq~r))]
-gnomG[ID==ID[1], frq_log10r_res := residuals(lm(meanFreq~log10r))]
-gnomG[ID==ID[1], cds_r_res := residuals(lm(cds_density~r))]
-gnomG[ID==ID[1], cds_log10r_res := residuals(lm(cds_density~log10r))]
-
-cortbl2G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_r_res", "cds_r_res"))]
-setnames(cortbl2G, old = oldnames, new = paste0(oldnames, ".meanFreq_cdsDensity.resid_r"))
-
-cortbl2.1G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_log10r_res", "cds_log10r_res"))]
-setnames(cortbl2.1G, old = oldnames, new = paste0(oldnames, ".meanFreq_cdsDensity.resid_log10r"))
-
-# for correlation between recombination and gene density, use straight up correlation
 cortbl3G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("r", "cds_density"))]
-setnames(cortbl3G, old = oldnames, new = paste0(oldnames, ".r_cdsDensity"))
+setnames(cortbl3G, old = oldnames, new = paste0(oldnames, ".r_cds"))
 
-cortbl3.1G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("log10r", "cds_density"))]
-setnames(cortbl3.1G, old = oldnames, new = paste0(oldnames, ".log10r_cdsDensity"))
+# 
+# # for partial correlation of freq and recombination, regress each against cds and take residuals
+# gnomG[ID==ID[1], frq_cds_res := residuals(lm(meanFreq~cds_density))]
+# gnomG[ID==ID[1], r_cds_res := residuals(lm(r~cds_density))]
+# gnomG[ID==ID[1], log10r_cds_res := residuals(lm(log10r~cds_density))]
+# 
+# cortbl1.1G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_cds_res", "r_cds_res"))]
+# setnames(cortbl1.1G, old = oldnames, new = paste0(oldnames, ".meanFreq_r_prtl"))
+# 
+# cortbl1.2G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_cds_res", "log10r_cds_res"))]
+# setnames(cortbl1.2G, old = oldnames, new = paste0(oldnames, ".meanFreq_log10r_prtl"))
+# 
+# # for partial correlation of freq and cds, regress each against recomb
+# gnomG[ID==ID[1], frq_r_res := residuals(lm(meanFreq~r))]
+# gnomG[ID==ID[1], frq_log10r_res := residuals(lm(meanFreq~log10r))]
+# gnomG[ID==ID[1], cds_r_res := residuals(lm(cds_density~r))]
+# gnomG[ID==ID[1], cds_log10r_res := residuals(lm(cds_density~log10r))]
+# 
+# cortbl2G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_r_res", "cds_r_res"))]
+# setnames(cortbl2G, old = oldnames, new = paste0(oldnames, ".meanFreq_cdsDensity.resid_r"))
+# 
+# cortbl2.1G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("frq_log10r_res", "cds_log10r_res"))]
+# setnames(cortbl2.1G, old = oldnames, new = paste0(oldnames, ".meanFreq_cdsDensity.resid_log10r"))
+# 
+# # for correlation between recombination and gene density, use straight up correlation
+# cortbl3G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("r", "cds_density"))]
+# setnames(cortbl3G, old = oldnames, new = paste0(oldnames, ".r_cdsDensity"))
+# 
+# cortbl3.1G <- gnomG[ID==ID[1], gnom_cor_decomp(data = .SD, chromosome = "chr", signals = c("log10r", "cds_density"))]
+# setnames(cortbl3.1G, old = oldnames, new = paste0(oldnames, ".log10r_cdsDensity"))
 
-wavcorG <- merge(merge(merge(cortbl1G, cortbl1.1G), merge(cortbl2G, cortbl2.1G)), merge(cortbl3G, cortbl3.1G))
+#wavcorG <- merge(merge(merge(cortbl1G, cortbl1.1G), merge(cortbl2G, cortbl2.1G)), merge(cortbl3G, cortbl3.1G))
+
+wavcorG <- merge( merge(cortbl1G, cortbl2G),  cortbl3G)
+
 wavcorG[, units := "genetic"]
 
 
