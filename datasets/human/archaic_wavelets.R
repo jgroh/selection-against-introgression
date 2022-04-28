c86b7a4ed09a6f9bc8094037dd6d87cc662eab91
Jeff <jgroh@ucdavis.edu>
2022-04-28 00:41:22 -0700

    add R scripts to make bed files of physical and genetic windows for human

--- datasets/human/archaic_wavelets.R
+++ datasets/human/archaic_wavelets.R
@@ -7,11 +7,11 @@ source("~/workspace/gnomwav/R/variance_decomp.R")
 source("~/workspace/gnomwav/R/correlation_decomp.R")
 
 
-chr_files10kb <- dir(path = ".", pattern = "chr.*_frq_10kb_windows.txt")
-chr_filesM <- dir(path = ".", pattern = "chr.*_frq_genetic_windows.txt")
+chr_files1kb <- dir(path = "archaic_freqs/", pattern = "chr.*_frq_1kb_windows.txt", full.names=T)
+chr_filesM <- dir(path = "archaic_freqs/", pattern = "chr.*_frq_genetic_windows.txt", full.names=T)
 
-gnom10kb <- rbindlist(lapply(chr_files10kb, fread))
-#ggplot(gnom10kb, aes(x = rec, y = freq)) + geom_point()  
+gnom1kb <- rbindlist(lapply(chr_files1kb, fread))
+#ggplot(gnom1kb, aes(x = rec, y = freq)) + geom_point()  
 
 gnomM <- rbindlist(lapply(chr_filesM, fread))
 #ggplot(gnomM, aes(x = rec, y = freq)) + geom_point()  
@@ -21,28 +21,28 @@ gnomM <- rbindlist(lapply(chr_filesM, fread))
 
 # ===== wavelet variance, physical units =====
 
-wv10kb <- gnom10kb[, gnom_var_decomp(.SD, chromosome = "chr", signals = c("freq", "rec"), rm.boundary = T, avg.over.chroms = T)]
-wv10kb[, level := factor(level, levels = c(paste0("d", 1:14), paste0("s", 11:14), "chr"))]
+wv1kb <- gnom1kb[, gnom_var_decomp(.SD, chromosome = "chr", signals = c("freq", "rec"), rm.boundary = T, avg.over.chroms = T)]
+wv1kb[, level := factor(level, levels = c(paste0("d", 1:17), paste0("s", 15:17), "chr"))]
 
-lineData10kb <- wv10kb[grepl("d", level, fixed = T)]
-ggplot(wv10kb, 
+lineData1kb <- wv1kb[grepl("d", level, fixed = T)]
+ggplot(wv1kb, 
        #aes(x = level, y = variance.freq, group = 1)) + 
-      aes(x = level, y = propvar.freq, group = 1)) + 
+       aes(x = level, y = propvar.freq, group = 1)) + 
   geom_point(size = 2.2) + 
-  geom_line(data = lineData10kb) + 
+  geom_line(data = lineData1kb) + 
   theme_classic() + 
-  scale_x_discrete(breaks = c(paste0("d", 1:14), paste0("s", 11:14), "chr"), 
-                   labels = c(as.character(1:14), paste0(11:14, " (scaling var)"), "chromosome"))   + 
-  labs(x = expression(Scale: log[2] ("10kb")), 
+  scale_x_discrete(breaks = c(paste0("d", 1:17), paste0("s", 15:17), "chr"), 
+                   labels = c(as.character(1:17), paste0(15:17, " (scaling var)"), "chromosome"))   + 
+  labs(x = expression(Scale: log[2] ("1kb")), 
        #y = "Variance") + 
        y = "Proportion of genome-wide variance") + 
   theme(aspect.ratio = 1, 
         axis.title = element_text(size = 15),
-      axis.text.x = element_text(angle = 90, size = 12),
-      axis.text.y = element_text(size = 12),
-      axis.line.x = element_blank())  +
-  geom_segment(aes(x = 0, xend = 14, y = -Inf, yend = -Inf)) + 
-  geom_segment(aes(x = 15, xend = 18, y = -Inf, yend = -Inf))  
+        axis.text.x = element_text(angle = 90, size = 12),
+        axis.text.y = element_text(size = 12),
+        axis.line.x = element_blank())  +
+  geom_segment(aes(x = 0, xend = 17, y = -Inf, yend = -Inf)) + 
+  geom_segment(aes(x = 18, xend = 20, y = -Inf, yend = -Inf))  
 
 
 
@@ -63,7 +63,7 @@ ggplot(wv10kb,
 # ===== wavelet variance, genetic units =====
 
 wvM <- gnomM[, gnom_var_decomp(.SD, chromosome = "chr", signals = c("freq", "rec"), rm.boundary = T, avg.over.chroms = T)]
-wvM[, level := factor(level, levels = c(paste0("d", 1:13), paste0("s", 10:13), "chr"))]
+wvM[, level := factor(level, levels = c(paste0("d", 1:17), paste0("s", 15:17), "chr"))]
 
 lineDataM <- wvM[grepl("d", level, fixed = T)]
 ggplot(wvM, 
@@ -72,8 +72,8 @@ ggplot(wvM,
   geom_point(size = 2.2) + 
   geom_line(data = lineDataM) + 
   theme_classic() + 
-  scale_x_discrete(breaks = c(paste0("d", 1:13), paste0("s", 10:13), "chr"), 
-                   labels = c(as.character(-12:0), paste0(-2:1, " (scaling var)"), "chromosome"))   + 
+  scale_x_discrete(breaks = c(paste0("d", 1:17), paste0("s", 15:17), "chr"), 
+                   labels = c(as.character(-16:0), paste0(-2:0, " (scaling var)"), "chromosome"))   + 
   labs(x = expression(Scale: log[2] (Morgan)), 
        #y = "Variance") + 
        y = "Proportion of genome-wide variance") + 
@@ -82,8 +82,8 @@ ggplot(wvM,
         axis.text.x = element_text(angle = 90, size = 12),
         axis.text.y = element_text(size = 12),
         axis.line.x = element_blank())  +
-  geom_segment(aes(x = 0, xend = 13, y = -Inf, yend = -Inf)) + 
-  geom_segment(aes(x = 14, xend = 17, y = -Inf, yend = -Inf))  
+  geom_segment(aes(x = 0, xend = 17, y = -Inf, yend = -Inf)) + 
+  geom_segment(aes(x = 18, xend = 20, y = -Inf, yend = -Inf))  
 
 # all chromosomes separately 
 # wvM_chr <- gnomM[, gnom_var_decomp(.SD, chromosome = "chr", signals = c("freq", "rec"), rm.boundary = T, avg.over.chroms = F)]
@@ -98,61 +98,61 @@ ggplot(wvM,
 # ===== Correlations =====
 
 # ---- physical scale 
-wc10kb <- gnom10kb[, gnom_cor_decomp(.SD, chromosome = "chr", signals = c("freq", "rec"), rm.boundary = T)]
-wc10kb[, level := factor(level, levels = c(paste0("d", 1:14), paste0("s", 11:14), "chr"))]
+wc1kb <- gnom1kb[, gnom_cor_decomp(.SD, chromosome = "chr", signals = c("freq", "rec"), rm.boundary = T)]
+wc1kb[, level := factor(level, levels = c(paste0("d", 1:17), paste0("s", 15:17), "chr"))]
 
-ggplot(wc10kb, aes(x = level, y = cor_jack)) + geom_point() + 
+ggplot(wc1kb, aes(x = level, y = cor_jack)) + geom_point() + 
   geom_errorbar(aes(ymin=lower95ci, ymax = upper95ci)) + 
   theme_classic() + 
-  scale_x_discrete(breaks = c(paste0("d", 1:14), paste0("s", 11:14), "chr"), 
-                   labels = c(as.character(1:14), paste0(11:14, " (scaling var)"), "chromosome"))   + 
-  labs(x = expression(Scale: log[2] ("10kb")), 
+  scale_x_discrete(breaks = c(paste0("d", 1:17), paste0("s", 15:17), "chr"), 
+                   labels = c(as.character(1:17), paste0(15:17, " (scaling var)"), "chromosome"))   + 
+  labs(x = expression(Scale: log[2] ("1kb")), 
        y = "Correlation (archaic allele freq, recomb)") + 
   theme(aspect.ratio = 1, 
         axis.title = element_text(size = 15),
         axis.text.x = element_text(angle = 90, size = 12),
         axis.text.y = element_text(size = 12),
         axis.line.x = element_blank())  +
-  geom_segment(aes(x = 0, xend = 14, y = -Inf, yend = -Inf)) + 
-  geom_segment(aes(x = 15, xend = 18, y = -Inf, yend = -Inf))  
+  geom_segment(aes(x = 0, xend = 17, y = -Inf, yend = -Inf)) + 
+  geom_segment(aes(x = 18, xend = 20, y = -Inf, yend = -Inf))  
 
 
 # ---- genetic scale 
 wcM <- gnomM[, gnom_cor_decomp(.SD, chromosome = "chr", signals = c("freq", "rec"), rm.boundary = T)]
-wcM[, level := factor(level, levels = c(paste0("d", 1:13), paste0("s", 10:13), "chr"))]
+wcM[, level := factor(level, levels = c(paste0("d", 1:17), paste0("s", 15:17), "chr"))]
 
 
-ggplot(wcM, aes(x = level, y = cor_n)) + geom_point() + 
+ggplot(wcM, aes(x = level, y = cor_jack)) + geom_point() + 
   geom_errorbar(aes(ymin=lower95ci, ymax = upper95ci)) + 
   theme_classic() + 
-  scale_x_discrete(breaks = c(paste0("d", 1:13), paste0("s", 10:13), "chr"), 
-                   labels = c(as.character(-12:0), paste0(-2:1, " (scaling var)"), "chromosome"))   + 
+  scale_x_discrete(breaks = c(paste0("d", 1:17), paste0("s", 15:17), "chr"), 
+                   labels = c(as.character(1:17), paste0(15:17, " (scaling var)"), "chromosome"))   +
   labs(x = expression(Scale: log[2] (Morgan)), 
-       y = "Variance") + 
+       y = "Correlation (archaic allele freq, recomb)") + 
   theme(aspect.ratio = 1, 
         axis.title = element_text(size = 15),
         axis.text.x = element_text(angle = 90, size = 12),
         axis.text.y = element_text(size = 12),
         axis.line.x = element_blank())  +
-  geom_segment(aes(x = 0, xend = 13, y = -Inf, yend = -Inf)) + 
-  geom_segment(aes(x = 14, xend = 17, y = -Inf, yend = -Inf))  
+  geom_segment(aes(x = 0, xend = 17, y = -Inf, yend = -Inf)) + 
+  geom_segment(aes(x = 18, xend = 20, y = -Inf, yend = -Inf))  
 
 
 
 # ===== Contribution to Correlation ====
 
 # ---- physical -----
-allWav10kb <- merge(wv10kb, wc10kb)
-allWav10kb[, contribution := cor_jack*sqrt(propvar.freq*propvar.rec)]
-allWav10kb[, level := factor(level, levels = c(paste0("d", 1:14), paste0("s", 11:14), "chr"))]
+allWav1kb <- merge(wv1kb, wc1kb)
+allWav1kb[, contribution := cor_jack*sqrt(propvar.freq*propvar.rec)]
+allWav1kb[, level := factor(level, levels = c(paste0("d", 1:17), paste0("s", 15:17), "chr"))]
 
-ggplot(allWav10kb) + 
+ggplot(allWav1kb) + 
   geom_bar(aes(fill = cor_jack, x = level, y = contribution), stat= "identity", color = "black") +
   scale_fill_gradient2(low = "blue", mid = "white", high = "red") +
-  scale_x_discrete(breaks = c(paste0("d", 1:14), paste0("s", 11:14), "chr"), 
-                   labels = c(as.character(1:14), paste0(11:14, " (scaling)"), "chromosome")) +
- theme_classic() +
-  labs(x = expression(Scale: log[2] ("10kb")), 
+  scale_x_discrete(breaks = c(paste0("d", 1:17), paste0("s", 15:17), "chr"), 
+                   labels = c(as.character(1:17), paste0(15:17, " (scaling)"), "chromosome"))   +
+  theme_classic() +
+  labs(x = expression(Scale: log[2] ("1kb")), 
        y = "Contribution to total correlation", 
        fill = "Correlation") + 
   theme(aspect.ratio = 1, 
@@ -164,14 +164,14 @@ ggplot(allWav10kb) +
 # ----- genetic ----
 allWavM <- merge(wvM, wcM, by = "level")
 allWavM[, contribution := cor_jack*sqrt(propvar.freq*propvar.rec)]
-allWavM[, level := factor(level, levels = c(paste0("d", 1:13), paste0("s", 10:13), "chr"))]
+allWavM[, level := factor(level, levels = c(paste0("d", 1:17), paste0("s", 15:17), "chr"))]
 
 
 ggplot(allWavM) + 
   geom_bar(aes(fill = cor_jack, x = level, y = contribution), stat= "identity", color = "black") +
   scale_fill_gradient2(low = "blue", mid = "white", high = "red") +
-  scale_x_discrete(breaks = c(paste0("d", 1:13), paste0("s", 10:13), "chr"), 
-                   labels = c(as.character(-12:0), paste0(-2:1, " (scaling)"), "chromosome"))   + 
+  scale_x_discrete(breaks = c(paste0("d", 1:17), paste0("s", 15:17), "chr"), 
+                   labels = c(as.character(1:17), paste0(15:17, " (scaling)"), "chromosome"))   +
   labs(x = expression(Scale: log[2] (Morgan)), 
        y = "Contribution to total correlation", 
        fill = "Correlation") + 
@@ -182,4 +182,16 @@ ggplot(allWavM) +
         axis.text.y = element_text(size = 12))
 
 
+# ===== Look at chromosome-level correlation
 
+chrMeans <- gnom10kb[, lapply(.SD, mean), .SDcols = c("rec", "freq"), by = chr]
+chrLen <- gnom10kb[, .(nwindow = .N), by = chr]
+chrMeans <- merge(chrMeans, chrLen)
+ggplot(chrMeans, aes(x = rec, y = freq, size = nwindow)) + geom_point() + geom_text(aes(label = chr), hjust=-.5,vjust=0) +
+  theme_classic()
+
+chrMeansM <- gnomM[, lapply(.SD, mean), .SDcols = c("rec", "freq"), by = chr]
+chrLenM <- gnomM[, .(nwindow = .N), by = chr]
+chrMeansM <- merge(chrMeansM, chrLenM)
+ggplot(chrMeansM, aes(x = rec, y = freq, size = nwindow)) + geom_point() + geom_text(aes(label = chr), hjust=-.5,vjust=0) +
+  theme_classic()
---
+++ datasets/human/homo_make_genetic_windows_bed.R
@@ -0,0 +1,18 @@
+library(data.table)
+
+rmap <- fread("recomb-hg38/genetic_map_GRCh38_merged.tab")
+rmap[, Morgan := pos_cm/100]
+
+bedM <- rmap[chrom != "chrX", .(Morgan = seq(min(Morgan), max(Morgan), by = 2^-16)), by = chrom]
+
+bedM[, wstart := Morgan - 0.5*2^-16]
+bedM[, wend := wstart + 2^-16]
+
+bedM[, bp_start := round(approx(x = rmap[chrom == .BY, Morgan], 
+                                y = rmap[chrom == .BY, pos], 
+                                xout = bedM[chrom == .BY, wstart])$y), by = chrom]
+bedM[, bp_end := round(approx(x = rmap[chrom == .BY, Morgan], 
+                              y = rmap[chrom == .BY, pos], 
+                              xout = bedM[chrom == .BY, wend])$y), by = chrom]
+
+fwrite(bedM[, .(chrom, bp_start, bp_end, Morgan)], file = "", quote=F, sep="\t", col.names=F)
