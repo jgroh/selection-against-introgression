1dc18bf0a90fe58c30aabb65b13d077a0f596048
Jeff <jgroh@ucdavis.edu>
2022-06-17 14:57:43 -0700

    add derived-allele-freq-general.py

---
+++ theory_and_simulations/code/derived-allele-freq-general.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# arguments to this script are (in order) file name, followed by generations of output from slim simulation
+import msprime, pyslim
+import tskit
+import numpy as np
+import pandas as pd
+import sys
+import os
+
+def allele_frequencies(ts, sample_sets=None):
+    if sample_sets is None:
+       sample_sets = [ts.samples()] 
+    n = np.array([len(x) for x in sample_sets])
+    def f(x):
+       return x / n
+    return ts.sample_count_stat(sample_sets, f, len(sample_sets), windows='sites', polarised=True, mode='site', strict=False)
+
+# Load the .trees file
+#ts = pyslim.load(sys.argv[1])
+ts = tskit.load("/Users/jeff/workspace/selection-against-introgression/theory_and_simulations/results/neutral_sims/equilibrium/replicate0.trees")
+#rep = float(os.path.basename(sys.argv[1]).lstrip('replicate').rstrip('.trees'))
+rep = 0
+
+
+# In[51]:
+
+
+
+
+
+# In[3]:
+
+
+frqs = []
+
+for gen in sys.argv[2:]: 
+    tm = max(sys.argv[2])-gen
+    allsets = [ts.samples(1, time = tm)]
+    af = allele_frequencies(ts, allsets)
+    allsites = np.array([s.position for s in ts.sites()])
+    frqsout = np.c_[np.tile(rep, len(allsites)), np.tile(gen, len(allsites)), allsites, af]
+    frqs.append(frqsout)
+
+fn = os.path.abspath(sys.argv[1]).rstrip('.trees')
+np.savetxt(fname=fn + "_ancestry.txt", X=np.vstack(frqs))
+
