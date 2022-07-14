b10e697f4ac1a63b7f555db72f7f244237570d92
Jeff <jgroh@ucdavis.edu>
2022-07-14 16:41:13 -0700

    fix format issue for msprime rec map

--- theory_and_simulations/code/make_variable_rec_map.R
+++ theory_and_simulations/code/make_variable_rec_map.R
@@ -9,20 +9,11 @@ for(i in 27:18){
   signal = signal + cos(x*2*pi/2^i)                                                                                         
 }
 
-plot(signal, cex = .5)
-lines(signal)
-
 signal = signal - min(signal)
 b = baseRate/mean(signal)
 r = b*signal
 
-plot(r, cex = 0.5); lines(r)
-length(r)
-
-length(x)
-ends <- x + 1e5-1
-
 out_slim <- c(paste(ends, collapse=","), paste(r, collapse=","))
-out_msprime <- c(paste( c(0, ends[1:(length(ends) - 1)], ends[length(ends)] + 1), collapse = "," ), paste(c(r,0.0), collapse=","))
+out_msprime <- c(format( paste( c(0, ends[1:(length(ends) - 1)], ends[length(ends)] + 1), collapse = "," ), scientific = F), paste(c(r,0.0), collapse=","))
 writeLines(out_slim, con = "variable_rec_map_slim.txt")
 writeLines(out_msprime, con = "variable_rec_map_msprime.txt")
